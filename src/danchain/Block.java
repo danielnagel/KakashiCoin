e99f59073b0ed5befc0c4a9601a769c191ac53f4
Daniel Nagel <danielsenpai95@gmail.com>
2020-02-04 07:41:40 +0100

    Chain validation and mining
      - Added Function to check if the chain is valid
      - Added mineBlock Method to Block Class
        - Proof of Work mining

--- src/danchain/Block.java
+++ src/danchain/Block.java
@@ -8,6 +8,7 @@ public class Block {
 	private String previousHash;
 	private String data;
 	private long timeStamp;
+	private int nonce;
 
 	public Block(String data, String previousHash) {
 		this.data = data;
@@ -25,7 +26,26 @@ public class Block {
 	}
 
 	public String calculateHash() {
-		String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+		String calculatedHash = StringUtil
+				.applySha256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
 		return calculatedHash;
+	}
+	
+	/**
+	 * In reality each miner will start iterating from a random point.
+	 * Some miners may even try random numbers for nonce.
+	 * Also it’s worth noting that at the harder difficulties solutions may require more than integer.MAX_VALUE,
+	 * miners can then try changing the timestamp.
+	 * 
+	 * @param difficulty
+	 */
+	public void mineBlock(int difficulty) {
+		// Create a string with difficulty * "0"
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		while(!hash.substring(0, difficulty).contentEquals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
 	}
 }
--- src/danchain/BlockChain.java
+++ src/danchain/BlockChain.java
@@ -7,14 +7,49 @@ import com.google.gson.GsonBuilder;
 public class BlockChain {
 
 	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
 
 	public static void main(String[] args) {
 
 		blockchain.add(new Block("Hi i'm the first block", "0"));
+		System.out.println("Trying to Mine block 1... ");
+		blockchain.get(blockchain.size() - 1).mineBlock(difficulty);
+		
 		blockchain.add(new Block("Hi i'm the second block", blockchain.get(blockchain.size() - 1).getHash()));
+		System.out.println("Trying to Mine block 2... ");
+		blockchain.get(blockchain.size() - 1).mineBlock(difficulty);
+		
 		blockchain.add(new Block("Hey i'm the third block", blockchain.get(blockchain.size() - 1).getHash()));
+		System.out.println("Trying to Mine block 3... ");
+		blockchain.get(blockchain.size() - 1).mineBlock(difficulty);
+		
+		System.out.println("\nBlockchain is Valid: " + isChainValid());
 
 		String blockchainJSON = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+		System.out.println("\nThe block chain: ");
 		System.out.println(blockchainJSON);
+	}
+	
+	public static Boolean isChainValid() {
+		Block currentBlock;
+		Block previousBlock;
+
+		for (int i = 1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i - 1);
+			
+			// compare registered hash and calculated hash
+			if(!currentBlock.getHash().equals(currentBlock.calculateHash())) {
+				System.out.println("Current Hashes not equal.");
+				return false;
+			}
+			
+			// compare previous hash and registered previous hash
+			if(!previousBlock.getHash().equals(currentBlock.getPreviousHash())) {
+				System.out.println("Previous Hashes not equal.");
+				return false;
+			}
+		}
+		return true;
 	}
 }
